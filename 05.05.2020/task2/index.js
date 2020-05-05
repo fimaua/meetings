ddeb5ef7be64def387eea049a728b5e255a5f2e5
Vitaly <nidastatachna@gmail.com>
2020-05-05 11:58:08 +0300

    js

--- 05.05.2020/task2/index.js
+++ 05.05.2020/task2/index.js
@@ -8,18 +8,18 @@
 //             age: 17,
 //         },
 //     }
-//     // const getCustomersList = obj => {
+// const getCustomersList = obj => {
 
-// //     const arr1 = Object.keys(obj)
-// //     const arr2 = Object.values(obj)
+//     const arr1 = Object.keys(obj)
+//     const arr2 = Object.values(obj)
 
-// //     return arr2.map(obj => {
-// //         const val = Object.assign({}, obj)
-// //         val['id'] = arr1[arr2.indexOf(obj)]
-// //         return val
-// //     }).sort((a, b) => a['age'] - b['age'])
+//     return arr2.map(obj => {
+//         const val = Object.assign({}, obj)
+//         val['id'] = arr1[arr2.indexOf(obj)]
+//         return val
+//     }).sort((a, b) => a['age'] - b['age'])
 
-// // }
+// }
 
 const getCustomersList = (obj) => {
     return Object.entries(obj).map(
