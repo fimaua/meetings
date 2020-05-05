87b6f06ffa8e68425e69f86efa023bfb7d677392
Vitaly <nidastatachna@gmail.com>
2020-05-05 11:55:32 +0300

    js

--- task5/undertask1/index.js
+++ task5/undertask1/index.js
@@ -1,32 +1,31 @@
-const obj1 = {
-        'customer-id-1': {
-            name: 'William',
-            age: 54,
-        },
-        'customer-id-2': {
-            name: 'Tom',
-            age: 17,
-        },
-    }
-    // const getCustomersList = obj => {
+// const customers = {
+//         'customer-id-1': {
+//             name: 'William',
+//             age: 54,
+//         },
+//         'customer-id-2': {
+//             name: 'Tom',
+//             age: 17,
+//         },
+//     }
+//     // const getCustomersList = obj => {
 
-//     const arr1 = Object.keys(obj)
-//     const arr2 = Object.values(obj)
+// //     const arr1 = Object.keys(obj)
+// //     const arr2 = Object.values(obj)
 
-//     return arr2.map(obj => {
-//         const val = Object.assign({}, obj)
-//         val['id'] = arr1[arr2.indexOf(obj)]
-//         return val
-//     }).sort((a, b) => a['age'] - b['age'])
+// //     return arr2.map(obj => {
+// //         const val = Object.assign({}, obj)
+// //         val['id'] = arr1[arr2.indexOf(obj)]
+// //         return val
+// //     }).sort((a, b) => a['age'] - b['age'])
 
-// }
+// // }
 
-function getCustomersList(obj) {
+const getCustomersList = (obj) => {
     return Object.entries(obj).map(
         (arr) => {
             let obj = arr[1];
             return Object.assign(obj, { id: arr[0] })
         }
     ).sort((a, b) => a.age - b.age)
-
 }
