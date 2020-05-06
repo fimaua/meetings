626e85a23138e6323f546c9c29f995ae83003391
Vitaly <nidastatachna@gmail.com>
2020-05-06 10:45:34 +0300

    js

--- 06.05.2020/task2/index.js
+++ 06.05.2020/task2/index.js
@@ -1,6 +1,6 @@
-function markAdmins(users, ids) {
+const markAdmins = (users, ids) =>
     users.map(user => {
+        let id = user.id
         const isAdmin = ids.includes(id);
         return {...user, isAdmin }
     });
-}
