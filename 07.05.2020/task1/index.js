de4fd2e045fbce918a0a0af22a77c8f0521962ad
Vitaly <nidastatachna@gmail.com>
2020-05-12 11:40:41 +0300

    js

---
+++ 07.05.2020/task1/index.js
@@ -0,0 +1,2 @@
+const compareObjects = (object1, object2) =>
+    JSON.stringify(object1) === JSON.stringify(object2)
---
+++ 12.05.2020/task1/index.js
@@ -0,0 +1,6 @@
+function makeCounter() {
+    let counter = 0;
+    return function count() {
+        return ++counter
+    }
+}
