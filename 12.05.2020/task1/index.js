22b9a65721b62a13a386b52b78207ef743514719
Vitaly <nidastatachna@gmail.com>
2020-05-12 11:48:52 +0300

    js

--- 12.05.2020/task1/index.js
+++ 12.05.2020/task1/index.js
@@ -1,6 +1,7 @@
-export function makeCounter() {
+function makeCounter() {
     let counter = 0;
     return function count() {
-        return ++counter
+        counter++
+        return counter;
     }
 }
