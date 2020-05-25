5cac7a8b360004755af0fb4508af6e36fc324d4e
Vitaly <nidastatachna@gmail.com>
2020-05-25 11:49:43 +0300

    js

---
+++ 25.05.2020/task1/index.js
@@ -0,0 +1,21 @@
+export class Sportsman {
+    constructor(name) {
+        this.name = name;
+    }
+    run() {
+        console.log(`${this.name} is running`)
+    }
+}
+
+
+
+export class Swimmer extends Sportsman {
+    constructor(name, style) {
+        super(name)
+        this.style = style;
+    }
+
+    swim() {
+        console.log(`${this.name} is swimming ${this.style}`)
+    }
+}
